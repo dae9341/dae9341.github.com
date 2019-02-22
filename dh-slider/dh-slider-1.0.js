22fc1f45811f939fd457ac5e6f39203fbc510ae6
kdh976 <dae9341@gmail.com>
2019-02-22 14:51:49 +0900

    ver1.0 1차 완료

--- dh-slider/dh-slider-1.0.js
+++ dh-slider/dh-slider-1.0.js
@@ -1,11 +1,11 @@
 var dh_slider = function dh_slider(opt) {
 
     opt = $.extend({
-        dh_slider:'#dh-bnrSlider',
-        dh_leftBtn:'',
-        dh_rightBtn:'',
-        dh_indicator:'',
-        firstSlide:1
+        dh_slider:'#dh-bnrSlider', //슬라이더 DOM
+        dh_leftBtn:'', //좌측버튼
+        dh_rightBtn:'', //우측버튼
+        dh_indicator:'', //인디케이터 DOM
+        firstSlide:1  // 시작페이지 설정 1~length
     },opt);
 
     var $dhSlider = $(opt.dh_slider); //#dh-bnrSlider
@@ -17,7 +17,7 @@ var dh_slider = function dh_slider(opt) {
     var intSliderCnt = $sliderLis.length; //슬라이드 갯수
     var slideWidth = $sliderLis[0].offsetWidth;
     var slideHeight = $sliderLis[0].offsetHeight;
-    var onIndexNum = 1;
+    var onIndexNum = 1; // 0~length
     var intStartSlide = opt.firstSlide;
     var cloneFlag = false;
     var intCloneItem;
@@ -57,27 +57,32 @@ var dh_slider = function dh_slider(opt) {
         onIndexNum = intStartSlide-1;
         $dhIndicator.find('a').eq(onIndexNum).addClass('on');
 
-        firstSlideSet(intStartSlide);
+        onSlideSet(intStartSlide);
 
-        //로드시 on화면 설정
-        function firstSlideSet(startLi) {
-            if(cloneFlag){
+    }
+
+    // 활성화 페이지 설정 startLi 1~length
+    function onSlideSet(startLi) {
+        if(cloneFlag){
+            if(startLi === 1){
+                $sliderLis.eq(1).clone(true).insertBefore($sliderLis.eq(0));
+            }else{
                 $sliderLis.eq(0).clone(true).appendTo($dhSliderUl);
-            }else {
-                $dhSliderUl.find('li').remove();
-                if (startLi === 1) {
-                    $dhSliderUlClone.find('li').eq(intSliderCnt - 1).clone(true).appendTo($dhSliderUl);
-                    for (var i = 0; i < intSliderCnt - 1; i++) {
-                        $dhSliderUlClone.find('li').eq(i).clone(true).appendTo($dhSliderUl);
-                    }
+            }
+        }else {
+            $dhSliderUl.find('li').remove();
+            if (startLi === 1) {
+                $dhSliderUlClone.find('li').eq(intSliderCnt - 1).clone(true).appendTo($dhSliderUl);
+                for (var i = 0; i < intSliderCnt - 1; i++) {
+                    $dhSliderUlClone.find('li').eq(i).clone(true).appendTo($dhSliderUl);
+                }
 
-                } else if (startLi > 1 && startLi <= intSliderCnt) {
-                    for (var i = startLi - 2; i < intSliderCnt; i++) {
-                        $dhSliderUlClone.find('li').eq(i).clone(true).appendTo($dhSliderUl);
-                    }
-                    for (var j = 0; j < startLi - 2; j++) {
-                        $dhSliderUlClone.find('li').eq(j).clone(true).appendTo($dhSliderUl);
-                    }
+            } else if (startLi > 1 && startLi <= intSliderCnt) {
+                for (var i = startLi - 2; i < intSliderCnt; i++) {
+                    $dhSliderUlClone.find('li').eq(i).clone(true).appendTo($dhSliderUl);
+                }
+                for (var j = 0; j < startLi - 2; j++) {
+                    $dhSliderUlClone.find('li').eq(j).clone(true).appendTo($dhSliderUl);
                 }
             }
         }
@@ -89,19 +94,31 @@ var dh_slider = function dh_slider(opt) {
             $dhSliderUl.animate({left:'+='+slideWidth+'px'});
 
 
-            //Clone필요한 index넘버 추출
+            //Clone필요한 index넘버 추출 (li의 위치조정)
             for(var i= 0; i < intSliderCnt; i++){
                 if($dhSliderUlClone.find('li').eq(i).find('img').attr('src') === $dhSliderUl.find('li').eq(intSliderCnt-1).find('img').attr('src')) {
                     intCloneItem = i;
                 }
             }
 
-            //이미지 2개 일때
-            if(cloneFlag) {
+            if(cloneFlag){
+
                 if (intCloneItem === 0) {
                     intCloneItem += 1;
                 } else {
                     intCloneItem -= 1;
+                }
+
+                if(onIndexNum <= 0){
+                    onIndexNum = 1;
+                }else{
+                    onIndexNum--;
+                }
+            }else{
+                if(onIndexNum <= 0){
+                    onIndexNum = intSliderCnt-1;
+                }else{
+                    onIndexNum--;
                 }
             }
 
@@ -109,15 +126,12 @@ var dh_slider = function dh_slider(opt) {
             $dhSliderUl.find('li').eq(intSliderCnt-1).remove();
             $dhSliderUlClone.find('li').eq(intCloneItem).clone(true).insertBefore($dhSliderUl.find('li').eq(0));
 
-            if(onIndexNum <= 0){
-                onIndexNum = intSliderCnt-1;
-            }else{
-                onIndexNum--;
-            }
 
             //indicator
             $dhIndicator.find('a').removeClass('on');
             $dhIndicator.find('a').eq(onIndexNum).addClass('on');
+
+            console.log(onIndexNum);
 
             console.log('left!::::', onIndexNum);
         });
@@ -140,21 +154,41 @@ var dh_slider = function dh_slider(opt) {
                 } else {
                     intCloneItem -= 1;
                 }
+
+                if(onIndexNum >= intSliderCnt-2 ){
+                    onIndexNum = 0;
+                }else {
+                    onIndexNum++;
+                }
+            }else{
+                if(onIndexNum >= intSliderCnt-1 ){
+                    onIndexNum = 0;
+                }else {
+                    onIndexNum++;
+                }
             }
+
+
             //li태그 이동
             $dhSliderUl.find('li').eq(0).remove();
             $dhSliderUlClone.find('li').eq(intCloneItem).clone(true).appendTo($dhSliderUl);
-
-            if(onIndexNum >= intSliderCnt-1 ){
-                onIndexNum = 0;
-            }else {
-                onIndexNum++;
-            }
 
             //indicator
             $dhIndicator.find('a').removeClass('on');
             $dhIndicator.find('a').eq(onIndexNum).addClass('on');
         });
+
+        $dhIndicator.find('a').on('click', function () {
+            $dhIndicator.find('a').removeClass('on');
+            $(this).addClass('on');
+            onIndexNum = $(this).index();
+            onSlideSet(onIndexNum + 1);
+        });
+
+    }
+    
+    function slideRightMove(value) {
+
     }
 
     try {
