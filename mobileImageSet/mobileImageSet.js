369810308be969bc9d32596fcb23095fb2f3addd
kdh976 <dae9341@gmail.com>
2019-04-25 17:51:28 +0900

    마우스 이벤트 추가

--- mobileImageSet/mobileImageSet.js
+++ mobileImageSet/mobileImageSet.js
@@ -3,7 +3,8 @@ var mobileImageSet = function mobileImageSet(opt){
         imageLayer: $('#imageLayer'),
         btnImageLoad: $('#btnEnter'),
         imageURL: $('#imageUrl'),
-        canvas: $('#canvas')
+        canvas: $('#canvas'),
+
 
     },opt);
 
@@ -15,12 +16,14 @@ var mobileImageSet = function mobileImageSet(opt){
     var imageWidth = 0;
     var imageHeight = 0;
     var $canvas = opt.canvas;
+    var ctx = $canvas[0].getContext("2d");
+    var drawReady = false;
+    var startX, startY, endX, endY;
 
 
     //시작 함수
     function startFunc() {
         bindEvt();
-        canvasSet();
     }
 
     //이벤트 바인드
@@ -32,21 +35,51 @@ var mobileImageSet = function mobileImageSet(opt){
             if($ImageTag){
                 $ImageTag.remove();
             }
-            $imageLayer.append('<img src="'+imageURL+'"/>');
+            $imageLayer.append("<img src='"+imageURL+"'/>");
             $ImageTag = $imageLayer.find('img');
+
+            $ImageTag[0].onload=function(){
+                imageWidth = this.clientWidth;
+                imageHeight = this.clientHeight;
+                $canvas[0].width = imageWidth;
+                $canvas[0].height = imageHeight;
+
+                ctx.rect(44,38,108,123);
+                ctx.stroke();
+
+                $canvas.mousedown (function(event){
+                    startX = event.clientX;
+                    startY = event.clientY;
+                    console.log(event.clientX, event.clientY);
+                    ctx.beginPath();
+                    drawReady = true;
+                });
+
+                $canvas.mousemove(function (event) {
+                    if(drawReady){
+                        ctx.strokeRect(startX, startY, event.clientX, event.clientY);
+                    }
+                });
+
+                $canvas.mouseup (function(event){
+                    endX = event.clientX;
+                    endY = event.clientY;
+                    // ctx.rect(startX, startY, event.clientX, event.clientY);
+                    // ctx.stroke();
+                    console.log(event.clientX, event.clientY);
+                    drawReady = false;
+
+                });
+
+            }
+
+            console.log($imageLayer);
             console.log($ImageTag);
-            // $canvas.width = $ImageTag.width();
-            // $canvas.height = $ImageTag.height();
-            // console.log($canvas);
+
+
         });
     }
 
-    function canvasSet() {
-        if($ImageTag) {
-            console.log($ImageTag.width());
-        }
-
-    }
 
     startFunc();
 };
