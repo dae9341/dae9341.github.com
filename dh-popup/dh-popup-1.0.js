9e4f0be0acc760a890238b9c2fea574579fbafbb
kdh976 <dae9341@gmail.com>
2019-04-29 15:21:13 +0900

    팝업 수정

--- dh-popup/dh-popup-1.0.js
+++ dh-popup/dh-popup-1.0.js
@@ -1,7 +1,7 @@
 var dh_popup = function dh_popup(opt) {
     opt = $.extend({
         button:'.popupBtn',
-        popup:'.popup',
+        popup:'.popup'
     },opt);
 
     var $button = $(opt.button);
@@ -13,24 +13,32 @@ var dh_popup = function dh_popup(opt) {
         bindEvent();
     }
 
-    function dimLayerSet(popup){
+    function dimLayerSet(){
         dimLayer='<div class="dimLayer"></div>';
         $('body').append(dimLayer);
-        $('.dimLayer').append(popup);
+        $popup.append("<a href='javascript:void(0);' class='closeBtn'>닫기</a>");
+        $('.dimLayer').append($popup);
+
+        $('.closeBtn').on('click', function () {
+            $('.dimLayer').remove();
+            $(this).remove();
+        })
     };
 
-    function popupLayerSet(popup){
-        var popup_w = popup.width();
-        var popup_h = popup.height();
-        popup.css({'position':'absolute','left':'50%', 'top':'50%','margin-left':'-'+popup_w / 2+'px', 'margin-top': '-' + popup_h / 2 + 'px' });
+    function popupLayerSet(){
+        var popup_w = $popup.width();
+        var popup_h = $popup.height();
+        $popup.css({'position':'absolute','left':'50%', 'top':'50%','margin-left':'-'+popup_w / 2+'px', 'margin-top': '-' + popup_h / 2 + 'px' });
     }
 
     function bindEvent() {
         $button.on('click', function () {
-            popupLayerSet($popup);
-            dimLayerSet($popup);
+            popupLayerSet();
+            dimLayerSet();
             $popup.show();
-        })
+        });
+
+
     };
 
     init();
